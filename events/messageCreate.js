c012f3ed371fb8c913d0035fccbafd9f99a47ae5
Anna Gibes <gibesav@gmail.com>
2023-08-17 12:52:16 -0500

    fixes error handling

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,13 +5,14 @@ module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
 		if (message.author.bot) return;
+		console.log(message);
 		try {
 			if (filter.isProfane(message.content)) {
-				message.delete()
+				message.delete();
 				message.reply(`Hey there, server dwellers! It seems a couple of not-so-friendly words snuck into our chat - but no worries bot bot is here to keep things fun and friendly for everyone!\n` + message.author.username + ` meant to say: "` + filter.clean(message.content) + `"`);
 			}
 		} catch (error) {
-			console.error(`Error executing ${message.commandName}`);
+			console.error(`Error executing message moderation`);
 			console.error(error);
 		}
 	},
