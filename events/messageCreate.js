ce646c5899cc315018ecc954e0d793d7c6fa3e9d
Anna Gibes <gibesav@gmail.com>
2023-08-17 13:09:03 -0500

    deletes unmodified message without errors

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,11 +5,10 @@ module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
 		if (message.author.bot) return;
-		console.log(message);
 		try {
 			if (filter.isProfane(message.content)) {
+				await message.reply(`Hey there, server dwellers! It seems a couple of not-so-friendly words snuck into our chat - but no worries, bot bot is here to keep things fun and friendly for everyone!\n` + message.author.username + ` meant to say: "` + filter.clean(message.content) + `"`);
 				message.delete();
-				message.reply(`Hey there, server dwellers! It seems a couple of not-so-friendly words snuck into our chat - but no worries bot bot is here to keep things fun and friendly for everyone!\n` + message.author.username + ` meant to say: "` + filter.clean(message.content) + `"`);
 			}
 		} catch (error) {
 			console.error(`Error executing message moderation`);
