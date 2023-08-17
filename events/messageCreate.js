8fe27bd77ab809f86c0df61256116121aaf1d706
Anna Gibes <gibesav@gmail.com>
2023-08-17 12:49:29 -0500

    cleans up  messageCreate event

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,15 +4,11 @@ const Filter = require('bad-words'),
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
-        if (message.author.bot) return;
-		if (filter.isProfane(message.content)) {
-			message.delete()
-			message.reply(`Hey there, server dwellers! It seems a couple of not-so-friendly words snuck into our chat - but no worries bot bot is here to keep things fun and friendly for everyone!\n` + message.author.username + ` meant to say: "` + filter.clean(message.content) + `"`);
-		}
+		if (message.author.bot) return;
 		try {
-			if (message.content === 'asd') {
-				console.log('testing');
-				message.reply("Hi! What's your name?");
+			if (filter.isProfane(message.content)) {
+				message.delete()
+				message.reply(`Hey there, server dwellers! It seems a couple of not-so-friendly words snuck into our chat - but no worries bot bot is here to keep things fun and friendly for everyone!\n` + message.author.username + ` meant to say: "` + filter.clean(message.content) + `"`);
 			}
 		} catch (error) {
 			console.error(`Error executing ${message.commandName}`);
