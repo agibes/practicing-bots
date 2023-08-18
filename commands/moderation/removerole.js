7435e71969f3cb67783437a7a7aa4dc5f675db33
Anna Gibes <gibesav@gmail.com>
2023-08-18 18:35:14 -0500

    adds /command to remove roll + permission checks

--- commands/moderation/removerole.js
+++ commands/moderation/removerole.js
@@ -3,17 +3,17 @@ require('dotenv').config();
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('addrole')
-        .setDescription('Give a member role')
+        .setName('removerole')
+        .setDescription(`Remove a member's role`)
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
         .addUserOption(option=> 
             option.setName('target')
-                .setDescription('The member to give a role to')
+                .setDescription('The member to remove a role from')
                 .setRequired(true)
             )
         .addStringOption(option=>
             option.setName('role')
-            .setDescription('The role to give')
+            .setDescription('The role to remove')
             .setRequired(true)
             .addChoices(
                 {name: 'Noble', value: 'Noble'},
@@ -36,20 +36,22 @@ module.exports = {
         const member = await interaction.guild.members.fetch(target.id);
 
         if (member === interaction.guild.fetchOwner()) {
-            await interaction.editReply(`You can't change the guild owner's role!`)
+            await interaction.editReply(`You can't change the guild owner's role!`);
             return;
             }
 
-        if (member === interaction.member) {
-            await interaction.editReply(`You can't add a role to yourself!`);
+        const targetRoleRank = member.roles.highest.position;
+        const requestRoleRank = interaction.member.roles.highest.position;
+        if (targetRoleRank >= requestRoleRank) {
+            await interaction.editReply(`You can't remove a role from a user with the same/higher rank than you!`);
             return;
-            }
+            };
 
-        member.roles.add(roleclass);
-        if(member.roles.cache.some(r => r.name === role)) {
-            await interaction.editReply('Wow! ' + target.username + ' is now a ' + role + '!'); 
-            } else {
-            await interaction.editReply(`Sorry, I couldn't add that role to ` + target.username + ` or you.`)
+        member.roles.remove(roleclass);
+        if(member.roles.cache.some(r => r.name != role)) {
+            await interaction.editReply('Update: ' + target.username + ' is not a ' + role + ' anymore!'); 
+        } else {
+            await interaction.editReply(`Sorry, I couldn't remove that role for you.`);
             };
     },
 };
